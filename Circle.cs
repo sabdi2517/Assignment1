39d375e8ecaa62bd860c4df282a080481c465cf0
sabdi2517 <58014586+sabdi2517@users.noreply.github.com>
2020-02-10 19:56:49 -0500

    Completed Circle Class

---
+++ Circle.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAA01_PROG2070
+{
+    
+    class Circle
+    {
+        private int radius;
+
+        public Circle()
+        {
+            radius = 1;
+        }
+        public Circle(int userInput)
+        {
+            radius = userInput;
+            
+        }
+
+        public int GetRadius()
+        {
+            return radius;
+        }
+
+        public void SetRadius (int radius)
+        {
+            radius = this.radius;
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+    }
+}
